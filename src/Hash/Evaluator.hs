79b59100b43d7e3ad250d803cd465897224c2d94
Yusuke Sangenya <longinus.eva@gmail.com>
2019-03-01 14:07:20 +0900

    fork & wait

--- src/Hash/Evaluator.hs
+++ src/Hash/Evaluator.hs
@@ -4,12 +4,15 @@ module Hash.Evaluator(evalAST) where
 import System.Exit (ExitCode(..))
 import System.Posix.Process (forkProcess, executeFile, getProcessStatus, ProcessStatus(..))
 import Hash.ShellAST (ShellAST(..))
-import Hash.Utils (forkWait)
+import Hash.Utils (forkWait, waitPid)
 
 evalAST :: ShellAST -> IO ExitCode
 evalAST (Single cmd args) = do
   let searchPath = not ('/' `elem` cmd)
   let env = Nothing
-  -- フォークして実行．ExitCodeを返す
-  exitcode <- forkWait $ executeFile cmd searchPath args env
+  -- forkしてexec．
+  pid <- forkProcess $ do
+    executeFile cmd searchPath args env
+  -- forkしたプロセスの終了を待つ
+  exitcode <- waitPid pid
   return exitcode
